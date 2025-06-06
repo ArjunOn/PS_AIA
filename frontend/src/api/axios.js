38d27d2cda39d513d1c75909406db8c9a0186a91
ArjunOn <mannemm@mail.gvsu.edu>
2025-06-06 01:52:21 -0400

    Update frontend API baseURL to Render deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000", // Backend URL
+  baseURL: "https://ps-aia.onrender.com", // Backend URL
 });
 
 export default api;
